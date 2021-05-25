b5f8abbde00afa1d104661e440c0d9f3ee9fdb1e
Shivani <78358314+Shivani0701@users.noreply.github.com>
2021-05-25 21:13:25 +0530

    Add files via upload

---
+++ 1recursion.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int factorial(int n)
+{
+    int prevfact;
+    if(n != 1)
+    {
+     prevfact=n*factorial(n-1);
+
+    }
+    else
+    prevfact=1;
+    return prevfact;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int prevfact=factorial(n);
+    cout<<prevfact<<endl;
+    return 0;
+}
